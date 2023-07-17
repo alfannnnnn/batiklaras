ea1ae2732b1aa02621a038989791a4ef6a247450
newFoundKind <alfannurfaiz9@gmail.com>
2023-07-17 09:33:47 +0700

    fix img

--- src/utils/slider.js
+++ src/utils/slider.js
@@ -1,5 +1,5 @@
 const slider = [
-  { title: "slider-1", img: "~/src/assets/slider-1.jpg" },
+  { title: "slider-1", img: "../assets/slider-1.JPG" },
   { title: "slider-2", img: "../assets/slider-2.jpg" },
 ];
 
